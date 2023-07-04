19c76094e7c8687d3139857ea32efcef386f8e68
Brian Muhimbura <brianmigel@gmail.com>
2023-07-04 13:38:43 +0300

    Remove client from blacklist

--- app/scripts/controllers/blacklist/BlacklistController.js
+++ app/scripts/controllers/blacklist/BlacklistController.js
@@ -3,6 +3,7 @@
         BlacklistController: function (scope, routeParams, route, location, resourceFactory, http, $uibModal, API_VERSION, $timeout, $rootScope, Upload) {
             scope.blacklist = [];
             scope.actualBlacklist = [];
+            scope.showInactive = false;
 
             scope.formData={};
             scope.searchText;
@@ -17,6 +18,7 @@
                 var items = resourceFactory.blacklistResource.getAllBlacklistClients({
                     offset: ((pageNumber - 1) * scope.clientsPerPage),
                     limit: scope.clientsPerPage,
+                    status: scope.showInactive? 'INACTIVE' : 'ACTIVE',
                     searchText:scope.searchText
                 }, function (data) {
                     scope.totalClients = data.totalFilteredRecords;
@@ -27,6 +29,7 @@
             resourceFactory.blacklistResource.getAllBlacklistClients({
                 offset: 0,
                 limit: scope.clientsPerPage,
+                status: scope.showInactive? 'INACTIVE' : 'ACTIVE',
                 searchText:scope.searchText
             }, function (data) {
                 scope.totalClients = data.totalFilteredRecords;
@@ -37,7 +40,13 @@
                 location.path(path);
             }
 
+            scope.getActiveInactive = function () {
+                scope.showInactive = !scope.showInactive;
+                scope.getResultsPage(1);
+            }
+
         }
+
     });
 
     mifosX.ng.application.controller('BlacklistController', ['$scope', '$routeParams', '$route', '$location', 'ResourceFactory', '$http', '$uibModal', 'API_VERSION', '$timeout', '$rootScope', 'Upload', mifosX.controllers.BlacklistController]).run(function ($log) {
