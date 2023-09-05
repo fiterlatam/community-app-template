b275adb0e0692ed4743a1030f2c2d5b8a3fa1bbf
Brian Muhimbura <brianmigel@gmail.com>
2023-09-05 12:11:48 +0300

    Credit restructure implementation

--- app/scripts/controllers/loanAccount/RestructureCreditsController.js
+++ app/scripts/controllers/loanAccount/RestructureCreditsController.js
@@ -1,11 +1,10 @@
 (function (module) {
     mifosX.controllers = _.extend(module, {
-        RestructureCreditsController: function (scope, resourceFactory, routeParams, location, dateFilter) {
+        RestructureCreditsController: function (scope, resourceFactory, routeParams, location, dateFilter,$uibModal) {
             scope.clientId = routeParams.clientId;
             scope.formData = {};
             scope.outstandingBalance=0;
             scope.restructureData;
-            scope.formData.submittedOnDate = new Date();
 
             resourceFactory.restructurecreditsResource.template({clientId:scope.clientId,anotherResource:'template'},function(data){
 
@@ -20,12 +19,23 @@
             scope.computeTotalBalance = function () {
                 for (let i=0; i<scope.activeLoans.length; i++) {
                     if (scope.activeLoans[i].selected){
-                        console.log("going to update balance")
                         scope.outstandingBalance = scope.outstandingBalance+ Number(scope.activeLoans[i].summary.totalOutstanding)
                     }
                 }
 
-                console.log("updated: "+ scope.outstandingBalance)
+            };
+
+
+            scope.resolveDateTime = function (dateTime) {
+                if (dateTime){
+                    let year = dateTime[0]
+                    let month = dateTime[1].toString().padStart(2,'0');
+                    let date = dateTime[2].toString().padStart(2,'0');
+                    let hour = dateTime[3].toString().padStart(2,'0');
+                    let minute = dateTime[4].toString().padStart(2,'0');
+                    let seconds = dateTime[5].toString().padStart(2,'0');
+                    return ""+ year +"-"+ month+"-"+ date+" "+ hour+":"+ minute+":"+ seconds;
+                }
             };
 
             scope.submit = function () {
@@ -37,21 +47,58 @@
                     }
                 };
 
+                var disbursementDate = dateFilter(scope.formData.disbursementDate, scope.dft);
+
+
                 let formData = {
+                    ...this.formData,
                     clientId:scope.clientId,
-                    selectedLoanIds: selectedLoans
+                    selectedLoanIds: selectedLoans,
+                    disbursementDate: disbursementDate,
+                    outstandingBalance: scope.outstandingBalance,
+                    locale : scope.optlang.code,
+                    dateFormat: scope.dft
                 }
 
                 resourceFactory.restructurecreditsResource.save({clientId:scope.clientId},formData,function(data){
-
+                    location.path('/viewclient/' + scope.clientId);
                 });
 
 
             };
 
+            scope.processRequest = function (action) {
+                scope.action = action;
+                $uibModal.open({
+                    templateUrl: 'processRequest.html',
+                    controller: ProcessRequestCtrl
+                });
+            }
+
+            var ProcessRequestCtrl = function ($scope, $uibModalInstance) {
+                $scope.action = scope.action;
+
+                $scope.processRequest = function () {
+                    let formData = {
+                        requestId:scope.requestData.id,
+                        transactionDate : dateFilter(new Date(), scope.df),
+                        notes : scope.requestData.comments,
+                        locale : scope.optlang.code,
+                        dateFormat: scope.df
+                    };
+                    resourceFactory.restructurecreditsResource.save({clientId:scope.clientId, anotherresource: scope.action}, formData, function (data) {
+                        $uibModalInstance.close('delete');
+                        location.path('/viewclient/' + scope.clientId);
+                    });
+                };
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss('cancel');
+                };
+            };
+
         }
     });
-    mifosX.ng.application.controller('RestructureCreditsController', ['$scope', 'ResourceFactory', '$routeParams', '$location', 'dateFilter', mifosX.controllers.RestructureCreditsController]).run(function ($log) {
+    mifosX.ng.application.controller('RestructureCreditsController', ['$scope', 'ResourceFactory', '$routeParams', '$location', 'dateFilter','$uibModal', mifosX.controllers.RestructureCreditsController]).run(function ($log) {
         $log.info("RestructureCreditsController initialized");
     });
 }(mifosX.controllers || {}));
