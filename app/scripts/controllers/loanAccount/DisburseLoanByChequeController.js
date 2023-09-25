4cb45be84f331b85b1f6925d09e275e282654ab0
Julius Peter Oketayot <juliuspeter441@gmail.com>
2023-09-25 08:56:08 +0300

    FBR-307: Disburse Loans with Cheques

---
+++ app/scripts/controllers/loanAccount/DisburseLoanByChequeController.js
@@ -0,0 +1,72 @@
+(function (module) {
+    mifosX.controllers = _.extend(module, {
+        DisburseLoanByChequeController: function (scope, routeParams, route, location, resourceFactory, http, $uibModal, API_VERSION, $timeout, $rootScope, Upload) {
+            scope.isCollapsed = true;
+            scope.batchId = routeParams.batchId;
+            resourceFactory.chequeBatchResource.get({batchId: routeParams.batchId}, function (data) {
+                scope.batchData = data;
+                var accountName = scope.batchData.bankAccNo + ' - ' + scope.batchData.agencyName;
+                scope.accountName = accountName;
+                scope.batchData.accountName = scope.accountName;
+                scope.bankAccId = scope.batchData.bankAccId;
+                resourceFactory.chequeBatchTemplateResource.get({bankAccId: scope.bankAccId}, function (data) {
+                    scope.statusOptions = data.statusOptions;
+                });
+            });
+
+            var DeleteChequeBatchController = function ($scope, $uibModalInstance) {
+                $scope.delete = function () {
+                    resourceFactory.chequeBatchResource.delete({batchId: scope.batchId }, function (data) {
+                      $uibModalInstance.close('delete');
+                      location.path('/viewchequeaccount/' + scope.bankAccId);
+                    });
+                };
+                $scope.cancel = function () {
+                    $uibModalInstance.dismiss('cancel');
+                };
+            };
+
+            scope.deleteChequeBatch = function () {
+                 $uibModal.open({
+                    templateUrl: 'deleteChequeBatch.html',
+                    controller: DeleteChequeBatchController
+                });
+            };
+
+            scope.routeTo = function (chequeId){
+                location.path('/viewdetails/' + scope.batchId + '/cheque/' + chequeId);
+            };
+
+            scope.cheques = [];
+            scope.actualCheques = [];
+            scope.formData = {};
+            scope.searchResults = [];
+            scope.chequesPerPage = 20;
+            scope.getResultsPage = function (pageNumber) {
+               resourceFactory.searchChequeResource.get({
+                    offset: ((pageNumber - 1) * scope.chequesPerPage),
+                    limit: scope.chequesPerPage,
+                    paged: 'true',
+                    orderBy: 'chequeNo',
+                    sortOrder: 'ASC',
+                    batchId: scope.batchId,
+                    chequeNo: scope.formData.chequeNo,
+                    status: scope.formData.status
+                }, function (data) {
+                    scope.totalCheques = data.totalFilteredRecords;
+                    scope.cheques = data.pageItems;
+               });
+            }
+            scope.getResultsPage(1);
+
+            scope.search = function () {
+                 scope.getResultsPage(1);
+            }
+
+        }
+    });
+
+    mifosX.ng.application.controller('DisburseLoanByChequeController', ['$scope', '$routeParams', '$route', '$location', 'ResourceFactory', '$http', '$uibModal', 'API_VERSION', '$timeout', '$rootScope', 'Upload', mifosX.controllers.DisburseLoanByChequeController]).run(function ($log) {
+        $log.info("DisburseLoanByChequeController initialized");
+    });
+}(mifosX.controllers || {}));
