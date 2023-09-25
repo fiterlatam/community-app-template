fc3b7422d991b96539fb1147032f6be857f82075
Leonardo Hildt <108893281+leonardohildt@users.noreply.github.com>
2023-09-25 14:51:02 -0300

    Release/1.0.11 prepare (#119)
    Merge the latest changes from Dev into UAT

---
+++ app/scripts/controllers/bank/bankcheques/ViewChequeAccountController.js
@@ -0,0 +1,18 @@
+(function (module) {
+    mifosX.controllers = _.extend(module, {
+        ViewChequeAccountController: function (scope, routeParams, route, location, resourceFactory, http, $uibModal, API_VERSION, $timeout, $rootScope, Upload) {
+            resourceFactory.bankAccountResource.get({bankAccountId: routeParams.accountId}, function (data) {
+                scope.bankAccountData = data;
+                var accountName = scope.bankAccountData.accountNumber + ' - ' + scope.bankAccountData.agency.name;
+                scope.accountName = accountName;
+            });
+            scope.routeTo = function (batchId) {
+                 location.path('/viewchequebatch/' + batchId);
+            };
+        }
+    });
+
+    mifosX.ng.application.controller('ViewChequeAccountController', ['$scope', '$routeParams', '$route', '$location', 'ResourceFactory', '$http', '$uibModal', 'API_VERSION', '$timeout', '$rootScope', 'Upload', mifosX.controllers.ViewChequeAccountController]).run(function ($log) {
+        $log.info("ViewChequeAccountController initialized");
+    });
+}(mifosX.controllers || {}));
