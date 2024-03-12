ac6c212c15fffdc779b38071091f386a9f40fe7d
Muhimbura Brian Mart <brianmigel@gmail.com>
2024-03-12 12:24:15 +0300

    fix fbr 626

--- app/scripts/controllers/prequalification/PrequalificationDetailsAnalysisController.js
+++ app/scripts/controllers/prequalification/PrequalificationDetailsAnalysisController.js
@@ -85,10 +85,13 @@
                 }
             }
             scope.policyCountColor = function (member) {
-                if (member.redValidationCount > 0) {
-                    return member.redValidationCount;
-                }else if(member.orangeValidationCount > 0||member.yellowValidationCount > 0){
-                    return Number(member.orangeValidationCount)+Number(member.yellowValidationCount);
+                let redValidationCount = member.redValidationCount||0;
+                let orangeValidationCount = member.orangeValidationCount || 0;
+                let yellowValidationCount = member.yellowValidationCount || 0;
+                if (redValidationCount > 0) {
+                    return Number(redValidationCount)+Number(orangeValidationCount)+Number(yellowValidationCount);
+                }else if(Number(orangeValidationCount) > 0 || yellowValidationCount > 0){
+                    return Number(orangeValidationCount)+Number(yellowValidationCount);
                 }else{
                     return '0'
                 }
