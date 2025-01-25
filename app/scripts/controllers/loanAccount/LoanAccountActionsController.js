2712745e5ca48cd6175f9460103d3e29ff7e795c
Brian Muhimbura <brianmigel@gmail.com>
2025-01-26 02:59:52 +0300

    fix/FSF-96-97-99 (#460)
    * FIX-FSF-96
    
    * waiver of interest fix
    
    * FIX-BACKDATE MANUAL TRANSACTIONS LIMIT

--- app/scripts/controllers/loanAccount/LoanAccountActionsController.js
+++ app/scripts/controllers/loanAccount/LoanAccountActionsController.js
@@ -439,10 +439,14 @@
                         }
                         scope.formData.transactionAmount = data.amount;
                         scope.formData[scope.modelName] = new Date();
+                        if(data.bankAccounts && data.bankAccounts.length > 0){
+                            scope.bankAccounts = data.bankAccounts;
+                        }
                     });
                     scope.title = 'label.heading.recoverypayment';
                     scope.labelName = 'label.input.transactiondate';
                     scope.isTransaction = true;
+                    scope.isRecoveryPayment = true;
                     scope.showAmountField = true;
                     scope.taskPermissionName = 'RECOVERYPAYMENT_LOAN';
                     break;
@@ -507,7 +511,8 @@
             };
 
             scope.calculateReceivableAmount = function () {
-                scope.formData.netAmountReceivable = Number(scope.formData.transactionAmount) - Number(scope.formData.collateralAmount);
+                if (scope.formData.netAmountReceivable && scope.formData.netAmountReceivable>0)
+                    scope.formData.netAmountReceivable = Number(scope.formData.transactionAmount) - Number(scope.formData.collateralAmount);
             };
 
             scope.addTranches = function () {
