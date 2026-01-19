819694939a57fab43415d6d2516aaa0c7cebb85a
brian <brianmigel@gmail.com>
2026-01-19 14:06:12 +0300

    Enhance document handling by adding fingerprint simulation, updating document descriptions, and improving download functionality

--- app/scripts/controllers/loanAccount/NewLoanAccAppController.js
+++ app/scripts/controllers/loanAccount/NewLoanAccAppController.js
@@ -795,7 +795,7 @@
                                             url: $rootScope.hostUrl + API_VERSION + '/paedocumentation/' + loanId + '/paedocument',
                                             data: {
                                                 name: guaranteeDocFile.name,
-                                                description: guaranteeDocFile.description,
+                                                description: `${guaranteeDocFile.name}(GUARANTEE_${j + 1})`,
                                                 categoryId: guaranteeDocFile.categoryId,
                                                 guaranteeNo: (j+1),
                                                 file: guaranteeDocFile.file
